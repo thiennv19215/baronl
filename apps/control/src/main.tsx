50d17c229ffdc0278cbbfe91d17a716f11b35155
thiennv19215 <114642828+thiennv19215@users.noreply.github.com>
2026-08-11 08:02:34 -0700

    make game store the control app entry

--- apps/control/src/main.tsx
+++ apps/control/src/main.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { GameHubRoot } from './features/games';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
     <GameHubRoot />
   </React.StrictMode>,
 );
