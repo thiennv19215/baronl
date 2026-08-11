4a847c12b008c1a96f9509fabcdeed1b86f5cbfb
thiennv19215 <114642828+thiennv19215@users.noreply.github.com>
2026-08-11 09:01:45 -0700

    Restore OrbitStage shell around Game Store

--- apps/control/src/main.tsx
+++ apps/control/src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 import { GameHubRoot } from './features/games';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <GameHubRoot />
+    <>
+      <App />
+      <GameHubRoot />
+    </>
   </React.StrictMode>,
 );
